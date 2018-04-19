cb3d942c36a5a7b2bf9382d0ac3fd6b87d089068
HCS <464962683@qq.com>
2018-04-19 22:43:38 +0800

    空参数和可选参数默认参数学习

---
+++ MyUnderstandingCSharp/_1_First/_1_One/_4_NullableProduct.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyUnderstandingCSharp._1_First._1_One
+{
+    /// <summary>
+    /// C# .net2 引入了?强制为null
+    /// 下面的查询用了C# .net3
+    /// </summary>
+    public class _4_NullableProduct
+    {
+        private readonly string bookName;
+        public string BookName
+        {
+            get { return bookName; }
+        }
+
+        decimal? price;
+        public decimal? Price
+        {
+            get { return price; }
+            private set { price = value; }
+        }
+
+        public _4_NullableProduct(string bookName,decimal? price)
+        {
+            this.bookName = bookName;
+            Price = price;
+        }
+
+
+        public void FindNull()
+        {
+            List<_4_NullableProduct> nullableProducts = new List<_4_NullableProduct>();
+            nullableProducts.Add(new _4_NullableProduct("AAA",1));
+            nullableProducts.Add(new _4_NullableProduct("BBB", 2));
+            nullableProducts.Add(new _4_NullableProduct("CCC", null));
+            nullableProducts.Add(new _4_NullableProduct("DDD", null));
+            nullableProducts.Add(new _4_NullableProduct("EEE", 3));
+            nullableProducts.FindAll(p => p.Price == null).ForEach(Console.WriteLine);
+        }
+    }
+}
---
+++ MyUnderstandingCSharp/_1_First/_1_One/_5_OptionalParameterProduct.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyUnderstandingCSharp._1_First._1_One
+{
+    /// <summary>
+    /// C# .net4 可选参数
+    /// </summary>
+    public class _5_OptionalParameterProduct
+    {
+
+
+        private readonly string bookName;
+        public string BookName
+        {
+            get { return bookName; }
+        }
+
+        decimal? price;
+        public decimal? Price
+        {
+            get { return price; }
+            private set { price = value; }
+        }
+
+        public _5_OptionalParameterProduct(string bookName, decimal? price = null)
+        {
+            this.bookName = bookName;
+            Price = price;
+        }
+
+
+        public void FindNull()
+        {
+            List<_5_OptionalParameterProduct> nullableProducts = new List<_5_OptionalParameterProduct>();
+            nullableProducts.Add(new _5_OptionalParameterProduct("AAA", 1));
+            nullableProducts.Add(new _5_OptionalParameterProduct("BBB", 2));
+            nullableProducts.Add(new _5_OptionalParameterProduct("CCC"));
+            nullableProducts.Add(new _5_OptionalParameterProduct("DDD"));
+            nullableProducts.Add(new _5_OptionalParameterProduct("EEE", 3));
+            nullableProducts.FindAll(p => p.Price == null).ForEach(Console.WriteLine);
+        }
+
+    }
+}
