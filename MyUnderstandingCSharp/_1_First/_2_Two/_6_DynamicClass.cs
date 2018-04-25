baf491a54193ef35dd37c4d4038b0b3dc688a3d5
HCS <464962683@qq.com>
2018-04-25 23:12:51 +0800

    动态的学习

---
+++ MyUnderstandingCSharp/_1_First/_2_Two/_6_DynamicClass.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyUnderstandingCSharp._1_First._2_Two
+{
+    public class _6_DynamicClass
+    {
+        /// <summary>
+        /// C# .net3仍然是静态语言 只是编译而已
+        /// </summary>
+        public void Test_Net_3()
+        {
+            var biJi = new { Name = "BiJi", Age = "1" };
+            var caiBi = new { Name = "CaiBi", Age = "2333",Skill="ChiFan" };
+        }
+
+        /// <summary>
+        /// C# .net4 用了dynmamic才是动态语言
+        /// </summary>
+        public void Test_Net_4()
+        {
+            dynamic o = "Hello";
+            Console.WriteLine(o.Length);
+            o = new string[] { "hi", "every", "one" };
+            Console.WriteLine(o.Length);
+        }
+    }
+}
