e2ba32093ca6ff1d56d847965990e8d50860a8ff
HCS <464962683@qq.com>
2018-04-26 23:12:14 +0800

    问号的学习

---
+++ MyUnderstandingCSharp/_1_First/_2_Two/_7_QuestionMark.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyUnderstandingCSharp._1_First._2_Two
+{
+    public class _7_QuestionMark
+    {
+        public void Test01()
+        {
+            int a = 4;
+            int b = -4;
+            int c = b > 0 ? b : a;//三元表达式
+        }
+
+        public void Test02()
+        {
+            int? a = null;//强制为null
+            if(a==null)
+            {
+                a = 5;
+                int y = a.Value;
+                Console.WriteLine(y);
+                a = null;
+            }
+            int z = a ?? 10; //null的联合操作符
+        }
+
+    }
+}
